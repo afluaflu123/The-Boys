55567079005c4959860160a0ff691208fb379eab
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 03:44:21 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -4,19 +4,18 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
 
-gif = [
-    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
-    'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
-    'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
-    'https://telegra.ph/file/293cc10710e57530404f8.mp4']
 
-@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel) & filters.private)
-async def autoapprove(client, message: ChatJoinRequest):
-        chat=message.chat 
-        user=message.from_user 
-        print("user isn't start bot(means group)")    
-        await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-        if APPROVED == "on":
+@Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
+async def approve(_, m : Message):
+    op = m.chat
+    kk = m.from_user
+    try:
+        add_group(m.chat.id)
+        await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await client.send_video(chat_id=chat.id,img, text=TEXT.format(mention=user.mention, title=chat.title))
-
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
+        add_user(kk.id)
+    except errors.PeerIdInvalid as e:
+        print("user isn't start bot(means group)")
+    except Exception as err:
+        print(str(err))    

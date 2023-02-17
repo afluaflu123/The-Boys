ffee418fcaaeaff0efb0619cebfe596a82ef8e0b
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 02:30:58 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -4,6 +4,20 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
 
+gif = [
+    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
+    'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
+    'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
+    'https://telegra.ph/file/293cc10710e57530404f8.mp4',
+    'https://telegra.ph/file/506898de518534ff68ba0.mp4',
+    'https://telegra.ph/file/dae0156e5f48573f016da.mp4',
+    'https://telegra.ph/file/3e2871e714f435d173b9e.mp4',
+    'https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4',
+    'https://telegra.ph/file/876edfcec678b64eac480.mp4',
+    'https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4',
+    'https://telegra.ph/file/b4834b434888de522fa49.mp4'
+]
+
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(_, m : Message):

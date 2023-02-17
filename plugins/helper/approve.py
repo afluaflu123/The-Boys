16048bbd846d76bda494b224dc4069fbec53d94d
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 02:29:56 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -5,11 +5,17 @@ from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
 
 
-@Client.on_chat_join_request((filters.group | filters.channel & ~filters.private))
-async def autoapprove(client, message: ChatJoinRequest):
-    chat=message.chat 
-    user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+@Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
+async def approve(_, m : Message):
+    op = m.chat
+    kk = m.from_user
+    try:
+        add_group(m.chat.id)
+        await app.approve_chat_join_request(op.id, kk.id)
+        img = random.choice(gif)
+        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
+        add_user(kk.id)
+    except errors.PeerIdInvalid as e:
+        print("user isn't start bot(means group)")
+    except Exception as err:
+        print(str(err))

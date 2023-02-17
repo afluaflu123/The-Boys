e18fca7e1bce709533ea8ddf2a771498a61a962b
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 03:31:02 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -4,13 +4,21 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
 
+gif = [
+    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
+    'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
+    'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
+    'https://telegra.ph/file/293cc10710e57530404f8.mp4']
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel) & filters.private)
 async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
+    print("user isn't start bot(means group)")
+    except Exception as err:
+    print(str(err))    
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        img = random.choice(gif)
+        await client.send_video(chat_id=chat.id,img, text=TEXT.format(mention=user.mention, title=chat.title))
 

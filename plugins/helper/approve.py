dd679d2baa7b4afd6ddcb0646bfb913c8b7a3cc6
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 02:20:50 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
 
 
-@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+@Client.on_chat_join_request((filters.group | filters.channel & ~filters.private)
 async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 

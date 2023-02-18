a117ad43c4788aa2286ee658f0149b9c142dd38b
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 13:22:44 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -9,7 +9,7 @@ from info import CHAT_ID, TEXT, APPROVED
 async def approve(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
-    print(f"{user.first_name} user isn't start bot(means group)")
+    print("user isn't start bot(means group)")
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))

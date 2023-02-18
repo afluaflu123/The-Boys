d7145158e4c3a38a72955c1a091a8d6fb6666f18
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-18 13:18:05 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -6,10 +6,10 @@ from info import CHAT_ID, TEXT, APPROVED
 
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel  & filters.private))
-async def autoapprove(client, message: ChatJoinRequest):
+async def approve(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
+    print(f"{user.first_name} user isn't start bot(means group)")
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))

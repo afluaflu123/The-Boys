237300cf7e35609eab8bcb76fe3259827d3eac40
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-19 20:28:43 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -20,14 +20,14 @@ gif = [
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(bot, Message):
-    op = message.chat
-    kk = message.from_user
+    chat=message.chat 
+    user=message.from_user 
     try:
         add_group(message.chat.id)
-        await bot.approve_chat_join_request(op.id, kk.id)
+        await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
         img = random.choice(gif)
-        await bot.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
-        add_user(kk.id)
+        await bot.send_video(user.id=img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
+        add_user(user.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
     except Exception as err:

abdd2b2d10d33435e1547cc4d01f10c9af815dad
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-19 20:16:41 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -5,10 +5,16 @@ from pyrogram.types import Message, User, ChatJoinRequest
 from info import TEXT, APPROVED 
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
-async def autoapprove(bot, message: ChatJoinRequest):
-    chat=message.chat 
-    user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
-    await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
-        await bot.send_message(chat_id=userid, text=TEXT.format(mention=user.mention, title=chat.title))
+async def approve(bot, Message):
+    op = m.chat
+    kk = m.from_user
+    try:
+        add_group(m.chat.id)
+        await bot.approve_chat_join_request(op.id, kk.id)
+        img = random.choice(gif)
+        await bot.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @SdBotz__**".format(m.from_user.mention, m.chat.title))
+        add_user(kk.id)
+    except errors.PeerIdInvalid as e:
+        print("user isn't start bot(means group)")
+    except Exception as err:
+        print(str(err))

d788231e7dec0a79b19e4e94eef8ca7e989fa84c
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-19 19:00:49 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -5,11 +5,11 @@ from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
 
 
-@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
-async def autoapprove(client, message: ChatJoinRequest):
+@Client.on_chat_join_request(filters.group & filters.channel & filters.private & filters.incoming)
+async def autoapprove(bot, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        await bot.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))

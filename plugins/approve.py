ba1a025cafe4cfe24d105d044ff2930b4887fb5d
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-21 20:39:50 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from pyrogram import Client, filters, errors
 from pyrogram.types import Message, User, ChatJoinRequest 
-from info import TEXT
+from info import TEXT, NANCY
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(client, message):
@@ -10,4 +10,4 @@ async def approve(client, message):
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    await client.send_message(chat_id=message.from_user.id, text=TEXT.format(mention=user.mention, title=chat.title))
+    await client.send_photo(chat_id=message.from_user.id, photo=random.choice(NANCY), text=TEXT.format(mention=user.mention, title=chat.title))

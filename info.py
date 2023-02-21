9a7bb9cbb9a16bb923c4e4f23c1f1690174c1cc1
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-21 20:02:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,6 +24,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/25c8bd93f8d5f787c188e.jpg https://telegra.ph/file/b1afff9eed3244abb9f39.jpg https://telegra.ph/file/39e246de80d814ff8bdfd.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/2581860836bb79b97d6d1.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2581860836bb79b97d6d1.jpg")
+TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour request has been approved")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]

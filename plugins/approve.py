085de8dc089b99dbe12dd2d1075e2ff01fffed34
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-21 21:26:30 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -1,9 +1,12 @@
 import os
-import asyncio
-import random
+import asyncio, random
 from pyrogram import Client, filters, errors
 from pyrogram.types import Message, User, ChatJoinRequest 
-from info import TEXT, NANCY
+from info import TEXT
+
+gif = [
+    'https://telegra.ph/file/39e246de80d814ff8bdfd.jpg'
+]
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(client, message):
@@ -11,4 +14,5 @@ async def approve(client, message):
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    await client.reply_photo(chat_id=message.from_user.id, photo=random.choice(NANCY), text=TEXT.format(mention=user.mention, title=chat.title))
+    img = random.choice(gif)
+    await client.send_message(chat_id=message.from_user.id,img, text=TEXT.format(mention=user.mention, title=chat.title))

e790a5f6642d18e64549956cb1f607036268dda3
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-22 22:47:48 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -4,8 +4,18 @@ from pyrogram import Client, filters, errors
 from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton
 from info import TEXT
 
-HACKER = [
-    'https://telegra.ph/file/39e246de80d814ff8bdfd.jpg'
+gif = [
+    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
+    'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
+    'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
+    'https://telegra.ph/file/293cc10710e57530404f8.mp4',
+    'https://telegra.ph/file/506898de518534ff68ba0.mp4',
+    'https://telegra.ph/file/dae0156e5f48573f016da.mp4',
+    'https://telegra.ph/file/3e2871e714f435d173b9e.mp4',
+    'https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4',
+    'https://telegra.ph/file/876edfcec678b64eac480.mp4',
+    'https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4',
+    'https://telegra.ph/file/b4834b434888de522fa49.mp4'
 ]
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
@@ -19,10 +29,9 @@ async def approve(client, message):
               ],[       
                 InlineKeyboardButton('💥 Jᴏɪɴ Mᴏᴠɪᴇs Cʜᴀɴɴᴇʟ 💥', url='https://t.me/Team_KL')
             ]]
-    await client.send_message(
+    await client.send_video(
         chat_id=message.from_user.id, 
-        photo="https://telegra.ph/file/51fdcccb41510ff8af8b1.jpg",
-        text=TEXT.format(mention=user.mention, title=chat.title),
+        img=random.choice(gif)
+        text="Hello {}!\nWelcome To {}\n\n__Powerd By".format(mention=user.mention, title=chat.title),
         reply_markup=InlineKeyboardMarkup(buttons)
         )
-          

138e9553f8440c8442da5c11dc6e5bb2d2c186bf
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-22 13:39:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1008,6 +1008,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.EXTRAMODS_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
+        )
 
     elif query.data == "about":
         buttons = [[

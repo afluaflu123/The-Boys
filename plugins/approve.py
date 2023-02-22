336257962da4f969a694c8d4e4575a0e94888c0c
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-22 18:35:35 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -1,7 +1,7 @@
 import os
 import asyncio, random
 from pyrogram import Client, filters, errors
-from pyrogram.types import Message, User, ChatJoinRequest 
+from pyrogram.types import Message, User, InlineKeyboardMarkup 
 from info import TEXT
 
 HACKER = [
@@ -14,4 +14,16 @@ async def approve(client, message):
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)   
-    await client.send_message(chat_id=message.from_user.id, text=TEXT.format(mention=user.mention, title=chat.title))
+    buttons = [[
+                InlineKeyboardButton('⚜ ᴄʜᴀɴɴᴇʟ', url='https://t.me/kerala_rockers'),        
+                InlineKeyboardButton('ɢʀᴏᴜᴘ ⚜️', url='https://t.me/+XiEBk6zT8RM5MjI9')
+            ],[
+                InlineKeyboardButton('🎭 ᴄʜᴀɴɴᴇʟ', url=f'https://t.me/+CeY_RGCtK1g0ZWQ9'),
+                InlineKeyboardButton('sʜᴀʀᴇ ᴍᴇ 🤝', url=f'https://t.me/share/url?url=https://t.me/Oru_adaar_Robot')
+            ]]
+    await client.send_message(
+        chat_id=message.from_user.id, 
+        text=TEXT.format(mention=user.mention, title=chat.title)
+        reply_markup=InlineKeyboardMarkup(buttons)
+        )
+          

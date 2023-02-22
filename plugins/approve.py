4462c3598e260fb3f1beba9104436d74f072c815
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-23 00:08:41 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -1,7 +1,7 @@
 import os
 import asyncio, random
 from pyrogram import Client, filters, errors
-from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton, ChatPermissions
 from info import TEXT
 
 PHOTOS = [

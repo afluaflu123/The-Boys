8bb8e77602056c4c71ded16391c3c792bb1a5d31
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-22 22:31:02 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -13,13 +13,13 @@ async def approve(client, message):
     chat=message.chat 
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)   
+    await message.approve_chat_join_request(chat_id=chat.id, user_id=user.id)   
     buttons = [[
                 InlineKeyboardButton('🔮 Jᴏɪɴ Mᴏᴠɪᴇs Gʀᴏᴜᴘ 🔮', url='https://t.me/KL_GROUP1')
               ],[       
                 InlineKeyboardButton('💥 Jᴏɪɴ Mᴏᴠɪᴇs Cʜᴀɴɴᴇʟ 💥', url='https://t.me/Team_KL')
             ]]
-    await client.send_photo(
+    await message.send_message(
         chat_id=message.from_user.id, 
         photo="https://telegra.ph/file/51fdcccb41510ff8af8b1.jpg",
         text=TEXT.format(mention=user.mention, title=chat.title),

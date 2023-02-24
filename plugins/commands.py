3e43de8b899cdd935565d37ddd139fcb57052ec0
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-24 11:54:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,6 +50,9 @@ async def start(client, message):
                     InlineKeyboardButton('😎 Aʙᴏᴜᴛ', callback_data='about')                                                      
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA") 
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),

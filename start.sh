1b2445cbcc39a164015e1637e0840d3c5f75cd9f
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-02-28 01:44:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hacker-Jr-TG/Team-KL.git /Team-KL
+  git clone https://github.com/Hacker-Jr-TG/The-Boys.git /Team-KL
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Team-KL

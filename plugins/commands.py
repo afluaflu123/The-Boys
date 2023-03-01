8346e117113bd00ec0517742acc7c511f8a65f09
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-03-01 21:07:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -111,12 +111,6 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-    kk, file_id = message.command[1].split("_", 1) if "_" in message.command[1] else (False, False)
-    pre = ('checksubp' if kk == 'filep' else 'checksub') if kk else False
-
-    status = await ForceSub(client, message, file_id=file_id, mode=pre)
-    if not status:
-        return
     data = message.command[1]
     if not file_id:
         file_id = data
